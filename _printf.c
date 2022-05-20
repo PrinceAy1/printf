92bbb355a801aa709934abd627f660ea5953ce7f
PrinceAy1 <102287613+PrinceAy1@users.noreply.github.com>
2022-05-20 23:17:36 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,74 +1,46 @@
-#include "main.h"
-
 /**
- * _printf - Parameters for printf
- * @format: list of arguments
- * Return: Printed thing
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
-
 int _printf(const char *format, ...)
 {
-	int chars;
-	va_list list;
+    int (*pfunc)(va_list, flags_t *);
+    const char *p;
+    va_list arguments;
+    flags_t flags = {0, 0, 0};
 
-	va_start(list, format);
-	if (format == NULL)
-		return (-1);
+    register int count = 0;
 
-	chars = charsFormats(format, list);
-
-	va_end(list);
-	return (chars);
-}
-
-/**
- * charsFormats - paremters printf
- * @format: list of arguments
- * @args: listing
- * Return: value of print
- */
-
-int charsFormats(const char *format, va_list args)
-{
-	int a, b, chars, r_val;
-
-	fmtsSpefier f_list[] = {{"c", _char}, {"s", _string},
-				{"%", _percent}, {"d", _integer}, {"i", _integer}, {NULL, NULL}
-	};
-	chars = 0;
-	for (a = 0; format[a] != '\0'; a++)
-	{
-		if (format[a] == '%')
-		{
-			for (b = 0; f_list[b].sym != NULL; b++)
-			{
-				if (format[a + 1] == f_list[b].sym[0])
-				{
-					r_val = f_list[b].f(args);
-					if (r_val == -1)
-						return (-1);
-					chars += r_val;
-					break;
-				}
-			}
-			if (f_list[b].sym == NULL && format[a + 1] != ' ')
-			{
-				if (format[a + 1] != '\0')
-				{
-					_putchar(format[a]);
-					_putchar(format[a + 1]);
-					chars = chars + 2;
-}
-				else
-					return (-1);
-			}
-		a += 1;
-		}
-		else
-		{
-			_putchar(format[a]);
-			chars++;
-		}
-	}
-	return (chars);
+    va_start(arguments, format);
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+        return (-1);
+    for (p = format; *p; p++)
+    {
+        if (*p == '%')
+        {
+            p++;
+            if (*p == '%')
+            {
+                count += _putchar('%');
+                continue;
+            }
+            while (get_flag(*p, &flags))
+                p++;
+            pfunc = get_print(*p);
+            count += (pfunc)
+                         ? pfunc(arguments, &flags)
+                         : _printf("%%%c", *p);
+        }
+        else
+            count += _putchar(*p);
+    }
+    _putchar(-1);
+    va_end(arguments);
+    return (count);
 }
